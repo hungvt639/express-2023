e50f93c5636315e2b756b2e46bc1cc031e368164
hungvt99 <hungvt@widogame.com>
2023-09-12 00:35:24 +0700

    config io

--- src/service/io.ts
+++ src/service/io.ts
@@ -5,7 +5,19 @@ import http from "http";
 class Io {
   io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
   init = (server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>) => {
-    this.io = new Server(server);
+    this.io = new Server(server, { cors: { origin: "*" } });
+
+    this.io.on("connection", (socket) => {
+      console.log(`User Connected: ${socket.id}`);
+
+      socket.on("join_room", (data) => {
+        socket.join(data);
+      });
+
+      socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+      });
+    });
     return this.io;
   };
 }
